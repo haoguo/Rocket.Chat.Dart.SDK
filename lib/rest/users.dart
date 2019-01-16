fec929970f0c75e3684541e45f5d3120ba6f8aff
Facundo Medica <facundolmedica@gmail.com>
2019-01-16 20:44:11 +0000

    added setCredentials and savePushToken

--- lib/rest/users.dart
+++ lib/rest/users.dart
@@ -20,4 +20,30 @@ abstract class _ClientUsersMixin implements _ClientWrapper {
     }).catchError((error) => completer.completeError(error));
     return completer.future;
   }
+
+  setCredentials(UserCredentials credentials) {
+    this._auth = _AuthInfo(credentials.id, credentials.token);
+  }
+
+  Future<dynamic> savePushToken(
+      String id, String token, String type, String appName) {
+    Completer<dynamic> completer = Completer();
+    http
+        .post('${_getUrl()}/push.token',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: json.encode(<String, String>{
+              'id': id,
+              'type': type,
+              'value': token,
+              'appName': appName,
+            }))
+        .then((response) {
+      _hackResponseHeader(response);
+      final data = json.decode(response.body)['data'];
+      completer.complete(data);
+    }).catchError((error) => completer.completeError(error));
+    return completer.future;
+  }
 }
